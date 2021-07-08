05f6f0601fea2cc6b3a6cea87f488ecd7144e786
WilhelmYakunin <v.jakunin@inbox.ru>
2021-07-08 22:02:41 +0300

    test

--- src/features/messages/Messages.jsx
+++ src/features/messages/Messages.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import _ from 'lodash';
 import { msgContainerStyles, msgStyles } from './messagesStyles';
 
 const Messages = () => {
-  const messages = useSelector((state) => state.messagesData.messages);
   const currentChannelId = useSelector((state) => state.channelsData.currentChannelId);
+  const messages = useSelector((state) => state.messagesData.messages);
   const channelMessages = messages.filter((message) => message.channelId === currentChannelId);
   const space = ': ';
 
+  // useEffect(() => {
+  //   const newMessage = messages[messages.length - 1];
+  //   console.log(newMessage);
+  // });
+
+  const renderMessages = () => channelMessages.map((messageInfo, index) => {
+    const { user, body } = messageInfo;
+    return (
+      <div key={index} className={msgStyles}>
+        <b>{user}</b>
+        {space}
+        {body}
+      </div>
+    );
+  });
+
   return (
     <div id="messages-box" className={msgContainerStyles}>
-      { channelMessages.length === 0 ? null
-        : channelMessages.map((messageInfo, index) => {
-          const { user, body } = messageInfo;
-          return (
-            <div key={_.uniqueId(index)} className={msgStyles}>
-              <b>{user}</b>
-              {space}
-              {body}
-            </div>
-          );
-        }) }
+      { channelMessages.length !== 0 && renderMessages() }
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
-
+import React from 'react';
+import { render } from 'react-dom';
 import '../assets/application.scss';
 import 'bootstrap';
 import App from './App.jsx';
 
-// App();
+render(<App />, document.getElementById('chat'));
 
 export default App;
