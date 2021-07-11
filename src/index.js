35730a5c0af599c53b7b5ea05415f63474c091ee
WilhelmYakunin <v.jakunin@inbox.ru>
2021-07-11 14:42:31 +0300

    'test'

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
-// import React from 'react';
-// import { render } from 'react-dom';
-// import { io } from 'socket.io-client';
+import React from 'react';
+import { render } from 'react-dom';
+import { io } from 'socket.io-client';
 import '../assets/application.scss';
 import 'bootstrap';
 import App from './App.jsx';
 
-// const socket = io();
-// App(socket).then((vdom) => render(vdom, document.getElementById('chat')));
+const socket = io();
+App(socket).then((vdom) => render(vdom, document.getElementById('chat')));
 
 export default App;
