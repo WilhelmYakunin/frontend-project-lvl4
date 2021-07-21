44b2bc6666d7893e524adb237f3bbe863bb025a0
WilhelmYakunin <v.jakunin@inbox.ru>
2021-07-21 23:04:19 +0300

    fix init

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,30 +4,18 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
 import initLocalization from '../locales/initLocalization';
-import { i18nLoaded, i18nError } from './i18nSlice';
 import AppHeader from '../features/AppHeader';
 import Login from '../features/login/Login';
 import Signup from '../features/signup/Signup';
 import NoMatch from '../components/NoMatch';
 import StartPage from '../features/StartPage';
 import Modal from '../features/modals/Switch';
-import LoadSpinner from '../components/LoadSpinner';
 
 const App = ({ socket }) => {
-  const dispatch = useDispatch();
-  const isI18nLoaded = useSelector((state) => state.i18n.isInit);
+  initLocalization();
 
-  initLocalization().then(() => {
-    try {
-      dispatch(i18nLoaded());
-    } catch (exception) {
-      dispatch(i18nError(exception.message));
-    }
-  });
-
-  return !isI18nLoaded ? <LoadSpinner /> : (
+  return (
     <Router>
       <React.Suspense fallback="Loading...">
         <div className="d-flex flex-column h-100">
