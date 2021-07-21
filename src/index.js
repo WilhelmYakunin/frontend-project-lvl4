230296b2aec58afd062b3231168b0841ab50eb61
WilhelmYakunin <v.jakunin@inbox.ru>
2021-07-21 22:59:40 +0300

    fix i18n loading

--- src/index.js
+++ src/index.js
@@ -4,17 +4,13 @@ import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 import ReactDOM from 'react-dom';
 import { io } from 'socket.io-client';
-import initLocalization from './locales/initLocalization.js';
 import '../assets/application.scss';
 import 'bootstrap';
 import init from './app/init.jsx';
 
 const socket = io();
 const app = init(socket);
-
-initLocalization().then(() => {
-  const container = document.getElementById('chat');
-  ReactDOM.render(app, container);
-});
+const container = document.getElementById('chat');
+ReactDOM.render(app, container);
 
 export default app;
